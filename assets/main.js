a9409d325603f31b347dbbc7fec31cc0e744d055
fusilli <piotrek@bardzofajny.net>
2019-06-10 13:19:14 +0200

    Added webfonts

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,8 @@
 import "./main.css";
+import PublicSansRegular from './fonts/public-sans/PublicSans-Regular.woff2';
+import PublicSansItalic from './fonts/public-sans/PublicSans-Italic.woff2';
+import PublicSansBold from './fonts/public-sans/PublicSans-Bold.woff2';
+import PublicSansBoldItalic from './fonts/public-sans/PublicSans-BoldItalic.woff2';
+import PublicSansBlack from './fonts/public-sans/PublicSans-Black.woff2';
+
+console.log('Działa to?');
--- assets/tailwind/tailwind.config.js
+++ assets/tailwind/tailwind.config.js
@@ -1,8 +1,29 @@
 module.exports = {
   theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      sans: [
+        '"Public Sans"',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ]
+    },
     extend: {
       colors: {
         'pomaranczowy': '#e82d0a',
+        'rozowy': '#df3b7a',
         'brazowy': '#63092c',
       }
     }
